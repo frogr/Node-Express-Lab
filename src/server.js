7ae6ba9a46e3a6af28f09c5cd59f30449bbf13fd
Austin F <austindanielfrench@gmail.com>
2017-11-13 15:25:56 -0800

    post adds a new object!! woohoo

--- src/server.js
+++ src/server.js
@@ -5,19 +5,31 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+const posts = [
+  {
+    title: 'pulp fiction',
+    content: 'two old-enough-to-know-better men shoot a lot of people'
+  },
+  {
+    title: 'lord of the rings',
+    content: 'a bunch of kids go on a journey with a ring'
+  }
+];
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 server.get('/posts', (req, res) => {
-  res.status(200).send('<h2> posts </h2>');
+  res.status(200).send(JSON.stringify(posts));
 });
 server.post('/posts', (req, res) => {
   const { title } = req.body;
   const { contents } = req.body;
-  res.send(`title: ${title} || content: ${content}`);
+  if (title !== undefined && contents !== undefined) {
+    posts.push(req.body);
+  }
+  res.send(`title: ${title} || content: ${contents}`);
 });
 
 const port = 8080;
